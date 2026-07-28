d0bd44eccede24bf7b3a752c652691bbbf80f95b
klu <1174445213@qq.com>
2026-07-29 01:03:55 +0800

    苍穹外卖代码

---
+++ sky-server/src/main/java/com/sky/controller/admin/DishController.java
@@ -0,0 +1,60 @@
+package com.sky.controller.admin;
+
+import com.sky.dto.DishDTO;
+import com.sky.dto.DishPageQueryDTO;
+import com.sky.result.PageResult;
+import com.sky.result.Result;
+import com.sky.service.DishService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+/*
+* 菜品管理
+* */
+@RestController
+@RequestMapping("/admin/dish")
+@Slf4j
+public class DishController {
+
+    @Autowired
+    private DishService dishService;
+
+    /*
+     * 新增菜品
+     * @param dishDTO
+     * */
+    @PostMapping()
+    public Result save(@RequestBody DishDTO dishDTO){
+        log.info("新增菜品：{}",dishDTO);
+        dishService.saveWithFlavor(dishDTO);
+        return Result.success();
+    }
+
+    /*
+     * 菜品分页查询
+     * @param dishPageQueryDTO
+     * */
+    @GetMapping("/page")
+    public Result<PageResult> page(DishPageQueryDTO dishPageQueryDTO){
+        log.info("菜品分页查询：{}",dishPageQueryDTO);
+        PageResult pageResult = dishService.pageQuery(dishPageQueryDTO);
+        return Result.success(pageResult);
+    }
+
+    /*
+     * 菜品的批量删除
+     * @param ids
+     * */
+    @DeleteMapping
+    public Result delete(@RequestParam List<Long> ids){
+        log.info("批量删除菜品：{}",ids);
+        dishService.deleteBatch(ids);
+
+        return Result.success();
+    }
+
+
+}
