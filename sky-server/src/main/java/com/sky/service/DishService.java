333024d7dc1ffcfe4686a8b955037f8a9a6f90c9
klu <1174445213@qq.com>
2026-08-07 02:19:42 +0800

    商品浏览业务功能代码开发

--- sky-server/src/main/java/com/sky/service/DishService.java
+++ sky-server/src/main/java/com/sky/service/DishService.java
@@ -49,5 +49,17 @@ public interface DishService {
     * */
     List<Dish> list(Long categoryId);
 
+
+    /*
+    * 菜品起售停售
+    * */
     void startOrStop(Integer status, Long id);
+
+
+    /**
+     * 条件查询菜品和口味
+     * @param dish
+     * @return
+     */
+    List<DishVO> listWithFlavor(Dish dish);
 }
