58dd1505dcf9fb3d0d5d4484832d946c9650a32f
klu <1174445213@qq.com>
2026-08-10 00:29:47 +0800

    导出运营数据

--- sky-server/src/main/java/com/sky/controller/admin/ReprotController.java
+++ sky-server/src/main/java/com/sky/controller/admin/ReprotController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletResponse;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 
@@ -80,6 +81,15 @@ public class ReprotController {
             @DateTimeFormat(pattern = "yyyy-MM-dd") LocalDate end){
         log.info("销量排名top10：{}，{}", begin,end);
         return Result.success(reportService.getSalesTop10(begin,end));
+    }
+
+    /*
+    * 导出运营数据报表
+    * @param response
+    * */
+    @GetMapping("/export")
+    public void export(HttpServletResponse response){
+        reportService.exportBusinessDate(response);
     }
 
 }
--- sky-server/src/main/java/com/sky/service/ReportService.java
+++ sky-server/src/main/java/com/sky/service/ReportService.java
@@ -6,6 +6,7 @@ import com.sky.vo.SalesTop10ReportVO;
 import com.sky.vo.TurnoverReportVO;
 import com.sky.vo.UserReportVO;
 
+import javax.servlet.http.HttpServletResponse;
 import java.time.LocalDate;
 
 public interface ReportService {
@@ -38,4 +39,10 @@ public interface ReportService {
     * @param end
     * */
     SalesTop10ReportVO getSalesTop10(LocalDate begin, LocalDate end);
+
+    /*
+    * 导出运营数据报表
+    * @param response
+    * */
+    void exportBusinessDate(HttpServletResponse response);
 }
