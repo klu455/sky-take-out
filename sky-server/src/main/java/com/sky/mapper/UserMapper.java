5d36dfa73744c304ebd9502e1c49a2ec10b65635
klu <1174445213@qq.com>
2026-08-09 21:14:44 +0800

    数据统计

--- sky-server/src/main/java/com/sky/mapper/UserMapper.java
+++ sky-server/src/main/java/com/sky/mapper/UserMapper.java
@@ -5,6 +5,8 @@ import com.sky.entity.User;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
 
+import java.util.Map;
+
 @Mapper
 public interface UserMapper {
 
@@ -23,9 +25,14 @@ public interface UserMapper {
 
     void insert(User user);
 
-    /*
-    *
-    * */
+
     @Select("select * from user where id = #{id}")
     User getById(Long userId);
+
+
+    /*
+    * 根据动态条件统计用户数量
+    * @param map
+    * */
+    Integer countByMap(Map map);
 }
